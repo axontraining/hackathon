da38739db3a0730df57c4a7d4898058808b5cbd0
chazaarg <chazarreta.patricio@axont.com>
2019-04-04 19:57:52 -0300

    build

---
+++ build/precache-manifest.427ab96e3f842344588533c67a4e695b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "82c6cc6019c0dd52cb88",
+    "url": "/static/js/main.8d7c62db.chunk.js"
+  },
+  {
+    "revision": "3c6b0af6e1b1dc731c70",
+    "url": "/static/js/2.7d487c8a.chunk.js"
+  },
+  {
+    "revision": "82c6cc6019c0dd52cb88",
+    "url": "/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "3c6b0af6e1b1dc731c70",
+    "url": "/static/css/2.db3bb1ca.chunk.css"
+  },
+  {
+    "revision": "3180ba9be51e40cad55e04aef971c6c8",
+    "url": "/index.html"
+  }
+];

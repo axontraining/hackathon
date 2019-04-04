ef52d8a66bd24f83e8eec460bb4d551394aecc8a
chazaarg <chazarreta.patricio@axont.com>
2019-04-04 18:21:12 -0300

    setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
-import Navbar from "./components/layout/Navbar"
-import Home from "./components/pages/home/Home"
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/home/Home";
 class App extends Component {
   render() {
     return (
       <div className="App">
-
-        <Navbar></Navbar>
-        <Home></Home>
+        <Navbar />
+        <Home />
       </div>
     );
   }
